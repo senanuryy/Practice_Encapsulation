72279c3f69c32c158e9bb1d1f86df1ba9b192422
Senanur Yavuzarslan <yavuzarslansenanur@gmail.com>
2025-01-13 22:12:28 +0300

    Add project files.

---
+++ Practice_Encapsulation/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Encapsulation
+{
+    internal class Car
+    {
+        private int _doorCount;
+        
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public int DoorCount
+        {
+            get
+            {
+                return _doorCount;
+            }
+            set
+            {
+                if (value == 2 || value == 4)
+                {
+                    _doorCount = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid door count! Please provide 2 or 4.");
+                    _doorCount = -1;
+                }
+            }
+        }
+         
+        public void CarInfo()
+        {
+            Console.WriteLine("Brand: " + Brand);
+            Console.WriteLine("Model: " + Model);
+            Console.WriteLine("Color: " + Color);
+            Console.WriteLine("DoorCount: " + DoorCount);
+        }
+    }
+}
---
+++ Practice_Encapsulation/Program.cs
@@ -0,0 +1,11 @@
+﻿using Practice_Encapsulation;
+
+Car car = new Car()
+{
+    Brand = "Tesla",
+    Color = "Black",
+    Model = "Model 3",
+    DoorCount = 4
+};
+
+car.CarInfo();
